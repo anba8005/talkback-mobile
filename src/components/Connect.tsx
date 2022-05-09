1296000c4a190683e4a37c44f41ebdcb82f46a83
Andrejus <anba8005@gmail.com>
2022-05-09 16:55:58 +0300

    small fix

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,7 +1,6 @@
 import { view } from '@risingstack/react-easy-state';
 import React, { useCallback } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { withTheme } from 'react-native-elements';
 import Fab from './Fab';
 import { useRootContext } from './RootContext';
 
@@ -10,6 +9,7 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		flex: 1,
 		alignItems: 'center',
+		backgroundColor: '#000000',
 	},
 	content: {
 		position: 'relative',
@@ -19,31 +19,30 @@ const styles = StyleSheet.create({
 		height: '100%',
 		justifyContent: 'center',
 		alignItems: 'center',
+		backgroundColor: '#000000',
 	},
 });
 
-export default withTheme(
-	view(function Connect() {
-		const root = useRootContext();
-		//
-		const handleConnect = useCallback(() => {
-			root.connect().catch(console.error);
-		}, [root]);
-		//
-		return (
-			<SafeAreaView style={styles.safe}>
-				<View style={styles.content} key="fullscreenView">
-					<Fab
-						icon="power-settings-new"
-						borderWidth={1}
-						size={200}
-						type="material"
-						positionH="center"
-						positionV="center"
-						onPress={handleConnect}
-					/>
-				</View>
-			</SafeAreaView>
-		);
-	}),
-);
+export default view(function Connect() {
+	const root = useRootContext();
+	//
+	const handleConnect = useCallback(() => {
+		root.connect().catch(console.error);
+	}, [root]);
+	//
+	return (
+		<SafeAreaView style={styles.safe}>
+			<View style={styles.content} key="fullscreenView">
+				<Fab
+					icon="power-settings-new"
+					borderWidth={1}
+					size={200}
+					type="material"
+					positionH="center"
+					positionV="center"
+					onPress={handleConnect}
+				/>
+			</View>
+		</SafeAreaView>
+	);
+});
